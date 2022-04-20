33b51e46df39d3a9e424337ff0b82660c78a1dfa
Jason Warren <warrenjason1@gmail.com>
2022-04-19 20:18:58 -0500

    "connected db, loading data from db now via views"

---
+++ main_app/db/create_goals.sql
@@ -0,0 +1,8 @@
+INSERT INTO main_app_goals(name, description, dailyz, duration,redeemed, created_at)
+VALUES ('Planks', '5 sets of 2 minutes each', 0, 30,False, '04-19-2022');
+INSERT INTO main_app_goals(name, description, dailyz, duration,redeemed, created_at)
+VALUES ('self-care', 'I did not use my phone after 9PM', 0,30,False, '04-19-2022');
+INSERT INTO main_app_goals(name, description, dailyz, duration,redeemed, created_at)
+VALUES ('well-watered', 'I drank a half gallon of water today', 0,30,False, '04-19-2022');
+INSERT INTO main_app_goals(name, description, dailyz, duration,redeemed, created_at)
+VALUES ('hit my step target', 'I hit my step target today', 0,30,False, '04-19-2022');
--- main_app/views.py
+++ main_app/views.py
@@ -3,30 +3,31 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from .models import Goals
 # Create your views here.
 class Home(TemplateView):
     template_name ="home.html"
 
-class Goal:
-    def __init__(self,name,description,dailyz,duration,redeemed):
-        self.name = name
-        self.description = description
-        self.dailyz = dailyz
-        self.duration = duration
-        self.redeemed = redeemed
+# class Goal:
+#     def __init__(self,name,description,dailyz,duration,redeemed):
+#         self.name = name
+#         self.description = description
+#         self.dailyz = dailyz
+#         self.duration = duration
+#         self.redeemed = redeemed
         #self.sponsor = sponsor
         #self.user = User.objects
     
-goals=[
-    Goal("Push-ups","10 sets a day","5","30","False"),
-    Goal("Meditate","30 minutes a day","10","30","False"),
-    Goal("Leetcode","hour a day","15","30","False"),
-]
+# goals=[
+#     Goal("Push-ups","10 sets a day","5","30","False"),
+#     Goal("Meditate","30 minutes a day","10","30","False"),
+#     Goal("Leetcode","hour a day","15","30","False"),
+# ]
 
 class GoalList(TemplateView):
     template_name='goallist.html'
 
     def get_context_data(self,**kwargs):
         context= super().get_context_data(**kwargs)
-        context["goals"]= goals
+        context["goals"]= Goals.objects.all()
         return context
