e06e49f250fd77f45ad4e9994ba9d4f46f7150a1
Jason Warren <warrenjason1@gmail.com>
2022-04-19 19:26:24 -0500

    "Added admin capabillities and views for goals db"

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Goals(models.Model):
+
+    name= models.CharField(max_length=40)
+    description= models.CharField(max_length=144)
+    dailyz= models.IntegerField()
+    duration= models.IntegerField()
+    redeemed= models.BooleanField()
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering=['dailyz']
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('goals/', views.GoalList.as_view(), name="goal-list"),
 ]
