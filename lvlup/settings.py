e73378c2345ed7d75164db152e294b50fdc8c27f
Jason Warren <warrenjason1@gmail.com>
2022-04-20 21:19:17 -0500

    testing env files

--- lvlup/settings.py
+++ lvlup/settings.py
@@ -21,6 +21,7 @@ load_dotenv()
 
 # dotenv_path = join(dirname(__file__), '.env')
 # load_dotenv(dotenv_path)
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
