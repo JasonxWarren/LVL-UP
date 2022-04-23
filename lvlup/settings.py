f48ba49c0ea2e348f21eb7b08a23007751f67aba
Jason Warren <warrenjason1@gmail.com>
2022-04-23 16:08:13 -0500

    config deployment

--- lvlup/settings.py
+++ lvlup/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
+if not os.environ.get('PRODUCTION'):
+    from dotenv import load_dotenv
+    load_dotenv()
 
 # import pathlib import Path
 # from os.path import join, dirname
@@ -27,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -37,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -140,5 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MEDIA_URL = "/media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-import django_heroku_
+import django_heroku
 django_heroku.settings(locals())
