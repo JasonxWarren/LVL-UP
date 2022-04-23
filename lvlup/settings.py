d784faeaba1ae5a1f07f56c12227984b32ecd6aa
Jason Warren <warrenjason1@gmail.com>
2022-04-23 15:45:57 -0500

    adding all

--- lvlup/settings.py
+++ lvlup/settings.py
@@ -139,3 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # MEDIA_URL = "/media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+import django_heroku_
+django_heroku.settings(locals())
