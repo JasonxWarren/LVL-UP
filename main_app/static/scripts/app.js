efddf49f9f90d2050b453ff2c6373f2dbb6fe5b2
Jason Warren <warrenjason1@gmail.com>
2022-04-26 17:14:45 -0500

    added logic for redeem button on template

---
+++ main_app/static/scripts/app.js
@@ -0,0 +1,9 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+$(document).ready(function() {
+    // $(".redeemed-btn").click(function(event){
+    //     let redeemed = False;
+    // })
+
+})
